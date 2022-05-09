b6d275b0330f732827a5e9a549078f72215a0422
sslivkoff <stormslivkoff@gmail.com>
2022-05-08 20:49:08 -0700

    convert setup.py to pyproject.toml

--- toolstr/__init__.py
+++ toolstr/__init__.py
@@ -1,3 +1,5 @@
+"""toolstr is a suite of str processing tools, including formatting and drawing"""
+
 from .charts import *
 from .blocks import *
 from .formatting import *
@@ -5,5 +7,5 @@ from .indents import *
 from .outlines import *
 from .spec import *
 
-__version__ = '0.1.3'
 
+__version__ = '0.2.0'
