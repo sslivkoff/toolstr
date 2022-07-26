8d8c171d6e8f9ac9f71b5f78b4346c3d8a69bbbf
sslivkoff <stormslivkoff@gmail.com>
2022-07-26 01:48:00 -0700

    add indent option to print()

--- toolstr/formats/rich_formats.py
+++ toolstr/formats/rich_formats.py
@@ -2,12 +2,22 @@ from __future__ import annotations
 
 import typing
 
+from .. import indents
+
 
 def print(
-    *text: typing.Any, style: typing.Optional[str] = None, **rich_kwargs: typing.Any
+    *text: typing.Any,
+    style: typing.Optional[str] = None,
+    indent: str | int | None = None,
+    **rich_kwargs: typing.Any,
 ) -> None:
     import rich.console
     import rich.theme
+
+    if indent is not None:
+        text = [indents.indent_block(str(text[0]), indent=indent)] + list(
+            text[1:]
+        )
 
     console = rich.console.Console(
         theme=rich.theme.Theme(inherit=False),
