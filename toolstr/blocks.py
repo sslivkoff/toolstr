d8e1a5c59929d50ce30d2ab23e2c13cf7bcaabbc
sslivkoff <stormslivkoff@gmail.com>
2022-08-11 23:41:47 -0700

    add gap argument to concatenate_blocks

--- toolstr/blocks.py
+++ toolstr/blocks.py
@@ -3,11 +3,20 @@ from __future__ import annotations
 import typing
 
 
-def concatenate_blocks(blocks: typing.Sequence[str]) -> str:
+def concatenate_blocks(
+    blocks: typing.Sequence[str | typing.Sequence[str]],
+    *,
+    gap: int | str | None = None,
+) -> str:
     """concatenate blocks of text horizontally"""
 
     # split blocks into lines
-    blocks_lines = [block.split('\n') for block in blocks]
+    blocks_lines: typing.MutableSequence[typing.Sequence[str]] = []
+    for block in blocks:
+        if isinstance(block, str):
+            blocks_lines.append(block.split('\n'))
+        else:
+            blocks_lines.append(block)
     n_lines = len(blocks_lines[0])
     for block_lines in blocks_lines:
         if len(block_lines) != n_lines:
@@ -15,9 +24,18 @@ def concatenate_blocks(blocks: typing.Sequence[str]) -> str:
                 'every block needs to have the same number of lines'
             )
 
+    if gap is None:
+        gap = ''
+    elif isinstance(gap, int):
+        gap = ' ' * gap
+    elif isinstance(gap, str):
+        pass
+    else:
+        raise Exception('unknown gap format: ' + str(type(gap)))
+
     # concatenate into new lines
     new_lines = []
     for pieces in zip(*blocks_lines):
-        new_lines.append(''.join(pieces))
+        new_lines.append(gap.join(pieces))
 
     return '\n'.join(new_lines)
