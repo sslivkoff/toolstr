78d14b316a2c72756ff5c8141ef8bf6431cacc9f
sslivkoff <stormslivkoff@gmail.com>
2022-12-13 23:06:22 -0800

    add unicode visuals and set relation symbols to set diff printer

--- toolstr/summaries/set_summary.py
+++ toolstr/summaries/set_summary.py
@@ -14,7 +14,8 @@ def print_set_diff(
     lhs_name: str = 'lhs',
     rhs_name: str = 'rhs',
     verbose: int = 1,
-    **kwargs: typing.Sequence[typing.Any]
+    include_visuals: bool = True,
+    **kwargs: typing.Sequence[typing.Any],
 ) -> None:
     if lhs is not None and rhs is not None:
         pass
@@ -36,17 +37,45 @@ def print_set_diff(
     intersection = sorted(lhs_set & rhs_set)
     union = sorted(lhs_set | rhs_set)
 
-    rows = [
+    if len(intersection) == len(lhs) and len(lhs) == len(rhs):
+        relation = lhs_name + ' = ' + rhs_name
+    elif len(lhs_only) == 0:
+        relation = lhs_name + ' ⊂ ' + rhs_name
+    elif len(rhs_only) == 0:
+        relation = lhs_name + ' ⊃ ' + rhs_name
+    elif len(intersection) == 0:
+        relation = lhs_name + ' ∩ ' + rhs_name + ' = Ø'
+    else:
+        relation = ''
+
+    rows: list[list[typing.Any]] = [
         [lhs_name, len(lhs_set)],
         [rhs_name, len(rhs_set)],
         [lhs_name + ' - ' + rhs_name, len(lhs_only)],
         [rhs_name + ' - ' + lhs_name, len(rhs_only)],
         [lhs_name + ' ∩ ' + rhs_name, len(intersection)],
         [lhs_name + ' ∪ ' + rhs_name, len(union)],
+        ['relation', relation],
     ]
 
+    if include_visuals:
+        visuals = _create_set_visuals(
+            lhs=lhs,
+            rhs=rhs,
+            lhs_only=lhs_only,
+            rhs_only=rhs_only,
+            union=union,
+        )
+        for visual, row in zip(visuals, rows):
+            row.append(visual)
+        rows[-1].append('')
+
+        labels = ['', 'size', 'visual']
+    else:
+        labels = ['', 'size']
+
     outlines.print_text_box('Sizes')
-    tables.print_table(rows)  # , labels=['', 'size'])
+    tables.print_table(rows, labels=labels)
 
     if verbose >= 2:
         outlines.print_text_box('{ ' + lhs_name + ' - ' + rhs_name + ' }')
@@ -82,4 +111,80 @@ def print_set_diff(
             add_row_index=True,
             justify='left',
         )
+
+
+def _create_set_visuals(
+    *,
+    lhs: typing.Sequence[typing.Any],
+    rhs: typing.Sequence[typing.Any],
+    lhs_only: typing.Sequence[typing.Any],
+    rhs_only: typing.Sequence[typing.Any],
+    union: typing.Sequence[typing.Any],
+    visual_width: int = 30,
+    left_cap: str = '├',
+    right_cap: str = '┤',
+) -> typing.Sequence[str]:
+
+    # left_cap = '←'
+    # right_cap = '→'
+    # left_cap = '└'
+    # right_cap = '┘'
+
+    visual_width = 30
+    n_per_char = len(union) / visual_width
+    if len(lhs) == 0:
+        n_left_chars = 1
+        lhs_visual = '│'
+    else:
+        n_left_chars = round(len(lhs) / n_per_char)
+        n_left_chars = max(2, n_left_chars)
+        lhs_visual = left_cap + '─' * (n_left_chars - 2) + right_cap
+    lhs_visual = lhs_visual.ljust(visual_width)
+
+    if len(rhs) == 0:
+        n_right_chars = 1
+        rhs_visual = '│'
+    else:
+        n_right_chars = round(len(rhs) / n_per_char)
+        n_right_chars = max(2, n_right_chars)
+        rhs_visual = left_cap + '─' * (n_right_chars - 2) + right_cap
+    rhs_visual = rhs_visual.rjust(visual_width)
+
+    if len(lhs_only) == 0:
+        lhs_only_visual = '│'
+    else:
+        n_lhs_only_chars = visual_width - n_right_chars + 1
+        lhs_only_visual = left_cap + '─' * (n_lhs_only_chars - 2) + right_cap
+    lhs_only_visual = lhs_only_visual.ljust(visual_width)
+
+    if len(rhs_only) == 0:
+        rhs_only_visual = '│'
+    else:
+        n_rhs_only_chars = visual_width - n_left_chars + 1
+        rhs_only_visual = left_cap + '─' * (n_rhs_only_chars - 2) + right_cap
+    rhs_only_visual = rhs_only_visual.rjust(visual_width)
+
+    left_intersection_pad = n_lhs_only_chars - 1
+    right_intersection_pad = n_rhs_only_chars - 1
+    n_intersection_chars = (
+        visual_width - left_intersection_pad - right_intersection_pad
+    )
+    intersection_visual = (
+        ' ' * left_intersection_pad
+        + left_cap
+        + '─' * (n_intersection_chars - 2)
+        + right_cap
+        + ' ' * right_intersection_pad
+    )
+
+    union_visual = left_cap + '─' * (visual_width - 2) + right_cap
+
+    return (
+        lhs_visual,
+        rhs_visual,
+        lhs_only_visual,
+        rhs_only_visual,
+        intersection_visual,
+        union_visual,
+    )
 
