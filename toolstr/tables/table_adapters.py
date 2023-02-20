a0617208826d7962df5b9b1261893e133e42d684
sslivkoff <stormslivkoff@gmail.com>
2023-02-20 15:16:22 -0800

    add support for printing polars dataframes as tables

--- toolstr/tables/table_adapters.py
+++ toolstr/tables/table_adapters.py
@@ -3,40 +3,67 @@ from __future__ import annotations
 import typing
 
 if typing.TYPE_CHECKING:
+    from typing_extensions import TypeGuard
     import pandas as pd
+    import polars as pl
 
 from . import table_utils
 
 
 def print_dataframe_as_table(
-    df: pd.DataFrame,
+    df: pl.DataFrame | pd.DataFrame,
     columns: typing.Sequence[typing.Any] | None = None,
     include_index: bool = True,
     **table_kwargs: typing.Any,
 ) -> str | None:
 
-    # promote index columns to plain columns
-    if include_index:
-        if df.index.name is not None:
-            name = df.index.name
+    if _is_polars_dataframe(df):
+        rows = df.rows()
+        columns = list(df.columns)
+
+    elif _is_pandas_dataframe(df):
+
+        # promote index columns to plain columns
+        if include_index:
+            if df.index.name is not None:
+                name = df.index.name
+            else:
+                name = 'index'
+            if columns is not None and name not in columns:
+                columns = [name] + list(columns)
+            df = df.reset_index()
+
+        # compile columns
+        if columns is not None:
+            # filter columns
+            df = df[[column for column in columns]]
         else:
-            name = 'index'
-        if columns is not None and name not in columns:
-            columns = [name] + list(columns)
-        df = df.reset_index()
+            # use all columns
+            columns = list(df.columns.values)
 
-    # compile columns
-    if columns is not None:
-        # filter columns
-        df = df[[column for column in columns]]
+        # convert to list of lists
+        rows = df.values.tolist()
+
     else:
-        # use all columns
-        columns = list(df.columns.values)
-
-    # convert to list of lists
-    rows = df.values.tolist()
+        raise Exception('unknown dataframe type: ' + str(type(df)))
 
     return table_utils.print_table(rows=rows, labels=columns, **table_kwargs)
+
+
+def _is_polars_dataframe(df: typing.Any) -> TypeGuard[pl.DataFrame]:
+    for parent in type(df).__mro__:
+        if parent.__module__.startswith('polars'):
+            return True
+    else:
+        return False
+
+
+def _is_pandas_dataframe(df: typing.Any) -> TypeGuard[pd.DataFrame]:
+    for parent in type(df).__mro__:
+        if parent.__module__.startswith('pandas'):
+            return True
+    else:
+        return False
 
 
 def print_dict_of_lists_as_table(
@@ -80,3 +107,4 @@ def print_list_of_dicts_as_table(
         rows.append(row)
 
     return table_utils.print_table(rows=rows, labels=keys, **table_kwargs)
+
